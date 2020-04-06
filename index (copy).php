35b193b177cf848971ba0ef1e2fdfa17ad998998
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-04-06 20:53:43 +0100

    creation of the backendSide branch

--- index (copy).php
+++ index (copy).php
@@ -1,3 +1,16 @@
+<?php  
+  
+session_start(); 
+   
+if(isset($_SESSION['views'])) 
+    $_SESSION['views'] = $_SESSION['views']+1; 
+else
+    $_SESSION['views']=1; 
+      
+echo"views = ".$_SESSION['views']; 
+  
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,14 +26,14 @@
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom fonts -->
+  <!-- Custom fonts for this template -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
-  <!-- Custom styles -->
+  <!-- Custom styles for this template -->
   <link href="css/agency.min.css" rel="stylesheet">
 
 </head>
